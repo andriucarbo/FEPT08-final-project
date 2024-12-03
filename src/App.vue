6fe8df9e2115d6feb061f7d2351b70ef8d0d79bb
Tu nombre <a.carbomarcos@gmail.com>
2024-12-03 10:33:20 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="text-8xl font-black text-emerald-800">{{ title }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name:"App",
+  data(){
+    return{
+      title:"holaaa! 🐷🐷 "
+    }
+  }
+  
+}
+</script>
+
+<style>
+  
+</style>
