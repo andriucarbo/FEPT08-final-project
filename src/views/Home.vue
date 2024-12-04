d458165d8d97e0821dd11e1cb045c6da91447101
Vanessa Cortés <vanessacortes@MacBook-Air-de-Vanessa.local>
2024-12-04 22:28:41 +0100

    borrador 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,100 @@
 <template>
-    <div>
-        <h2>Home view</h2>
+        <!-- Main -->
+<div class="main border-cyan-900 border-2" >
+    <div class="mainConocenos bg-amber-300 flex flex-col justify-center items-center">
+        <p>Conócenos</p>
+    </div>
+
+    <div class="mainHabitantes bg-cyan-200 flex flex-col justify-center items-center " >
+        <p> Habitantes</p>
+        <div class="grid grid-cols-4 bg-pink-500 gap-4 ml-4 mr-4" >
+            <div class="uno bg-slate-200" >
+                <img src="/AnimalesGridHomeView/burros1.png" alt="Imagen burros"/>
+                <p>Nombre</p>
+                <p>Historia</p>
+                <button>Apadríname</button>
+            </div>
+            <div class="dos bg-slate-200" >
+                <img src="/AnimalesGridHomeView/gallinas1.png" alt="imagen gallinas sobre silla">
+                <p>Nombre</p>
+                <p>Historia</p>
+                <button>Apadríname</button>
+            </div>
+            <div class="tres bg-slate-200" >
+                <img src="/AnimalesGridHomeView/pato1.png" alt="imagen pato blanco">
+                <p>Nombre</p>
+                <p>Historia</p>
+                <button>Apadríname</button>
+            </div>
+            <div class="cuatro bg-slate-200" >
+                <img src="/AnimalesGridHomeView/lechones1.png" alt="imagen 3 lechones">
+                <p>Nombre</p>
+                <p>Historia</p>
+                <button>Apadríname</button>
+            </div>
+            <div class="cinco bg-slate-200" >
+                <img src="/AnimalesGridHomeView/ternera1.png" alt="imagen ternera marrón">
+                <p>Nombre</p>
+                <p>Historia</p>
+                <button>Apadríname</button>
+            </div>
+            <div class="seis bg-slate-200" >
+                <img src="/AnimalesGridHomeView/oveja3.png" alt="oveja lana rizada">
+                <p>Nombre</p>
+                <p>Historia</p>
+                <button>Apadríname</button>
+            </div>
+            <div class="seis bg-slate-200" >
+                <img src="/AnimalesGridHomeView/conejo1.png" alt="imagen conejo marrón">
+                <p>Nombre</p>
+                <p>Historia</p>
+                <button>Apadríname</button>
+            </div>
+            <div class="seis bg-slate-200" >
+                <img src="/AnimalesGridHomeView/lechon2.png" alt="lechón con manchitas">
+                <p>Nombre</p>
+                <p>Historia</p>
+                <button>Apadríname</button>
+            </div>
+        </div>
         
     </div>
+
+    <div class="mainColabora bg-cyan-400 flex flex-col justify-center items-center" >
+        <p> Colabora</p>
+    </div>
+
+    <div class="mainEduca bg-green-200 flex flex-col justify-center items-center" >
+        <p>Educa</p>
+    </div>
+
+    <div class="mainReceta bg-green-400 flex flex-col justify-center items-center"  >
+        <p>Recetas</p>
+    </div>
+
+    <div class="mainNoticias bg-violet-300 flex flex-col justify-center items-center" >
+        <p>Noticias</p>
+    </div>
+
+    <div class="mainTienda bg-violet-500 flex flex-col justify-center items-center">
+        <p>Tienda</p>
+    </div>
+
+    <div class="mainContacto bg-teal-300 flex flex-col justify-center items-center" >
+        <p>Contacto</p>
+    </div>
+
+    <div></div>
+
+    <div></div>
+</div>
 </template>
 
 
 <script>
+
 export default {
-    name: "Home"
-    
+    name: "Home",
 }
 </script>
 
