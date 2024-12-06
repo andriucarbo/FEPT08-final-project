7f33e1968f13c478dc66f3c3366d187d4672dba4
Vanessa Cortés <vanessacortes@MacBook-Air-de-Vanessa.local>
2024-12-06 01:48:26 +0100

    borrador 6 vcortete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,14 @@
 
     <!-- Apartado home "conócenos - conoce nuestra historia" -->
     <div class="mainConocenos bg-[url('/HomePics/VacaPortada1.png')] bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center w-full h-full">
-        <h1 class="title1 text-5xl text-slate-50 my-20 mx-20">Fundación <br> Santuario PATOdalavida</h1>
-        <p class="text1 text-2xl text-slate-50">Proporcionamos un espacio seguro para los animales considerados de granja</p>
-        <p class="text1 text-2xl text-slate-50 mx-32 my-20 text-center">En 2024, Andrea y Vanessa dieron vida a PATOdalavida, un santuario pensado para ofrecer un entorno seguro y cálido a animales de granja. En este refugio, cada habitante encuentra un hogar lleno de amor y cuidado, donde pueden vivir tranquilos y felices. Andrea y Vanessa, con su dedicación y cariño, se aseguran de que todos los animales sean tratados con respeto.<br> PATOdalavida no solo es un refugio, sino también un lugar de descanso, donde la vida digna de cada animal es lo más importante. <br> Este santuario es un hermoso ejemplo de lo que se puede lograr con un corazón lleno de amor por los animales.</p>
-        <button class="buttonnuestraHistoria mb-20 text-[#857a5c]  rounded p-4 hover:bg-neutral-50 hover:text-[#857a5c] focus:outline-none">Conoce nuestra historia</button>
+        <div>
+            <h1 class="title1 text-5xl text-slate-50 my-10 mx-20">Fundación <br> Santuario PATOdalavida</h1>
+            <p class="text1 text-3xl text-slate-50 text-center">Proporcionamos un espacio seguro para los animales considerados de granja.</p>
+            <p class="text1 text-2xl text-slate-50 mx-10 my-10 text-center">En 2024, Andrea y Vanessa dieron vida a PATOdalavida, un santuario pensado para ofrecer un entorno seguro y cálido a animales de granja. En este refugio, cada habitante encuentra un hogar lleno de amor y cuidado, donde pueden vivir tranquilos y felices. Andrea y Vanessa, con su dedicación y cariño, se aseguran de que todos los animales sean tratados con respeto.<br> PATOdalavida no solo es un refugio, sino también un lugar de descanso, donde la vida digna de cada animal es lo más importante. <br> Este santuario es un hermoso ejemplo de lo que se puede lograr con un corazón lleno de amor por los animales.</p>
+            <div class="buttonContainer flex justify-end pr-20 pt-96 mb-20 mr-10">
+                <button class="buttonnuestraHistoria text-xl p-4 text-[#857a5c] rounded hover:bg-neutral-50 hover:text-[#857a5c] focus:outline-none">Conoce nuestra historia</button>
+            </div> 
+        </div> 
     </div>
 
     <!-- Apartado carrusel Nuestros valores -->
@@ -16,14 +20,21 @@
     </div>
 
     <!-- Apartado Conoce a nuestros habitantes -->
-    <div class="mainHabitantes bg-cyan-200 flex flex-col justify-center items-center " >
+    <div class="mainHabitantes bg-[#e2d9c7] flex flex-col justify-center items-center " >
         <h1> Nuestros habitantes</h1>
-        <CondadorDinamico/>
         <!-- <p>contador dinámico</p> -->
 
-        <div class="grid grid-cols-4 bg-pink-500 gap-4 ml-4 mr-4" >
-            <div class="uno bg-slate-200" >
-                <img src="/AnimalesGridHomeView/burros1.png" alt="Imagen burros"/>
+        <div class="CardHabitantes flex flex-wrap justify-center items-center w-full h-full mb-64">
+            <CardHabitantes/>
+            <div>
+                <button class="buttonnuestroshabitantes text-xl p-4 bg-[#857a5c] text-slate-50 rounded hover:bg-[#857a5c] hover:text-slate-50 focus:outline-none  ">Conoce a nuestros habitantes</button>
+            </div>
+        </div>
+
+        <div class="grid grid-cols-4 bg-pink-500 gap-4 ml-4 mr-4 " >
+            
+
+                <!-- <img src="/AnimalesGridHomeView/burros1.png" alt="Imagen burros"/>
                 <p>Nombre</p>
                 <p>Historia</p>
                 <button>Apadríname</button>
@@ -69,9 +80,9 @@
                 <p>Nombre</p>
                 <p>Historia</p>
                 <button>Apadríname</button>
-            </div>
-        </div>
+            </div> -->
         
+        </div>
     </div>
 
     <div class="mainColabora bg-cyan-400 flex flex-col justify-center items-center" >
@@ -82,41 +93,38 @@
         <p>Educa</p>
     </div>
 
-    <div class="mainReceta bg-green-400 flex flex-col justify-center items-center"  >
+    <!-- <div class="mainReceta bg-green-400 flex flex-col justify-center items-center"  >
         <p>Recetas</p>
-    </div>
+    </div> -->
 
-    <div class="mainNoticias bg-violet-300 flex flex-col justify-center items-center" >
+    <!-- <div class="mainNoticias bg-violet-300 flex flex-col justify-center items-center" >
         <p>Noticias</p>
-    </div>
+    </div> -->
 
     <div class="mainTienda bg-violet-500 flex flex-col justify-center items-center">
         <p>Tienda</p>
     </div>
 
-    <div class="mainContacto bg-teal-300 flex flex-col justify-center items-center" >
+    <!-- <div class="mainContacto bg-teal-300 flex flex-col justify-center items-center" >
         <p>Contacto</p>
-    </div>
+    </div> -->
 
-    <div></div>
-
-    <div></div>
 </div>
 </template>
 
 
 <script>
-import ContadorDinamico from './ContadorDinamico.vue';
+import CardHabitantes from './CardHabitantes.vue';
 export default {
     name: "Home",
-    components: { ContadorDinamico },
+    components: {CardHabitantes},
 }
 </script>
 
 
 <style>
 
-.title1, .text1 {
+.title1, .text1, .buttonnuestraHistoria  {
     font-family: 'Crimson Text', serif;
 }
 
