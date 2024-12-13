2ac23ea4b3bb4957e1ff8afe3d80e4af05fbd394
Vanessa Cortés <vanessacortes@MacBook-Air-de-Vanessa.local>
2024-12-13 17:33:37 +0100

    actualización firebase viernes tarde

--- src/views/Familia.vue
+++ src/views/Familia.vue
@@ -2,26 +2,38 @@
     <div>
         <h1>Family</h1>
         <ul>
-            <li class="flex mb-4" v-for="deadAnimal in deadAnimals"></li>
+            <li class="flex mb-4" v-for="deadAnimal in deadAnimals" :key="deadAnimal.id">
+                <p>{{ deadAnimal.name }}</p>
+                <!-- <img :src="{{ deadAnimal.image }}" :alt="">
+                <p>{{ deadAnimal.description }}</p> -->
+            </li>
         </ul>
+        <!-- mirar ejemplo Irene Favourites.vue si queremos añadir últimos animales vistos con el evento click -->
     </div>
 </template>
 
 
 <script>
-
+import { updateDatabaseFam, getDatabaseFam} from '../firebase.js'
 
 export default {
     name: "Familia",
     data(){
-        deadAnimals: []
+        return {
+            deadAnimals: []
+        };
+    },
+    mounted () {
+        this.getDatabaseFam();
     },
     methods: {
-        async getAnimals(){
+        async getDatabaseFam(){
             try{
                 const response = await fetch("/public/databaseFam.json")
-                // const data = await response.js()
-                this.animals = data;
+                const data = await response.json()
+                this.deadAnimals = data;
+                await
+                updateDatabaseFam(this.deadAnimals)
 
             }catch(error){
                 console.log(error)
